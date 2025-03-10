5204186aa5c4ccca98feb071e6dc109805b66f64
anene-achalu <a81108990@gmail.com>
2025-03-11 00:18:29 +0300

    i delete comments

--- smallest no.cpp
+++ smallest no.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    // Define the array
     int numbers[] = {120, 45, 67, 89, 34, 23, 90, 11};
-    int size = sizeof(numbers) / sizeof(numbers[0]); // Calculate the size of the array
-
-    // Assume the first element is the smallest
+    int size = sizeof(numbers) / sizeof(numbers[0]);
     int minNumber = numbers[0];
-
-    // Loop through the array to find the smallest number
     for (int i = 1; i < size; i++) {
         if (numbers[i] < minNumber) {
-            minNumber = numbers[i]; // Update the minimum value
+            minNumber = numbers[i];
         }
     }
-
-    // Display the smallest number
-    std::cout << "The smallest number in the array is: " << minNumber << std::endl;
+ cout<< "The smallest number in the array is: " << minNumber <<endl;
 
     return 0;
 }
